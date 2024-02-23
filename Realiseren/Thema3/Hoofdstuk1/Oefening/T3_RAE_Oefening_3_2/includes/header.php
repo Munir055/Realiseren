1b90a603240e4a736a69b4eb4265732a4107f5e5
Munir055 <munir.alshawi@edu-kw1c.nl>
2024-02-23 12:07:54 +0100

    T3_REA_Oefening_3_2

---
+++ Realiseren/Thema3/Hoofdstuk1/Oefening/T3_RAE_Oefening_3_2/includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+        <?php
+        echo 'T3_REA_Oefening3.2.php.';
+        ?>
+    </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="rea_3_2_munir.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <h1>
+        <?php
+            echo 'T3_REA_Oefening3.2.php';
+        ?>
+    </h1>
+</header>

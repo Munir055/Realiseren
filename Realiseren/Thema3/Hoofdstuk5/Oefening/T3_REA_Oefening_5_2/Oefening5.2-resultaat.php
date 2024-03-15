ab4c99f10f0a49be538e59f64e0567ceb6921b13
Munir055 <munir.alshawi@edu-kw1c.nl>
2024-03-15 11:59:55 +0100

    T3_REA_Oefening_5_2.

--- Realiseren/Thema3/Hoofdstuk5/Oefening/T3_REA_Oefening_5_2/Oefening5.2-resultaat.php
+++ Realiseren/Thema3/Hoofdstuk5/Oefening/T3_REA_Oefening_5_2/Oefening5.2-resultaat.php
@@ -1,1 +1,27 @@
 <?php
+/**
+ * User: Munir Alshawi
+ * Date: 15-3-24
+ * File: Oefening 5.2
+ */
+?>
+<?php
+// Definieer de juiste gebruikersnaam en wachtwoord
+$correctUsername = "Admin";
+$correctPassword = "P@ssword";
+
+// Ontvang de ingediende gebruikersnaam en wachtwoord uit het formulier
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+// Controleer of de gebruikersnaam en het wachtwoord overeenkomen met de juiste gegevens
+if ($username === $correctUsername && $password === $correctPassword) {
+	// als de gebruikersnaam en het wachtwoord correct zijn
+	echo "<h1>Welkom u bent ingelog</h1>";
+	echo "<p> <img src='imges/stickers-schatkist.jpg.jpg'></p>";
+} else {
+	//als de gebruikersnaam of het wachtwoord onjuist zijn
+	echo "<h1>Onjuiste gebruikersnaam of wachtwoord</h1>";
+	echo "<p> <a href='Oefening5_2.php'>Probeer het opnieuw.</a></p>";
+}
+
---
+++ Realiseren/Thema3/Hoofdstuk5/Oefening/T3_REA_Oefening_5_2/Oefening5_2.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * User: Munir Alshawi
+ * Date: 15-3-24
+ * File: Oefening 5.2
+ *
+ */
+?>
+<form action="Oefening5.2-resultaat.php" method="post">
+	<table>
+		<tr>
+			<td colspan="2">
+				<h1>Inloggen</h1>
+				<p>
+					Welkom op onze website. <br>
+					Na correct in te loggen, krijgt u toegang tot onze schatkist. Deze is dan voor u, om te gebruiken!
+				</p>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Username
+			</td>
+			<td>
+				<input type="text" name="username">
+			</td>
+		</tr>
+
+		<tr>
+			<td>
+				Password
+			</td>
+			<td>
+				<input type="password" name="password">
+			</td>
+		</tr>
+
+		<tr>
+			<td colspan="">
+				<input type="submit" value="Verzenden">
+			</td>
+		</tr>
+	</table>
+</form>
+

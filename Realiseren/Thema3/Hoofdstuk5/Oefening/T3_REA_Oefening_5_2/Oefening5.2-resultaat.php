63a8454906e4933e63563b21876353f01d2141c8
Munir055 <munir.alshawi@edu-kw1c.nl>
2024-03-15 12:03:29 +0100

    T3_REA_Oefening_5_2.

--- Realiseren/Thema3/Hoofdstuk5/Oefening/T3_REA_Oefening_5_2/Oefening5.2-resultaat.php
+++ Realiseren/Thema3/Hoofdstuk5/Oefening/T3_REA_Oefening_5_2/Oefening5.2-resultaat.php
@@ -15,11 +15,13 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 
 // Controleer of de gebruikersnaam en het wachtwoord overeenkomen met de juiste gegevens
-if ($username === $correctUsername && $password === $correctPassword) {
+if ($username == $correctUsername && $password == $correctPassword)
+{
 	// als de gebruikersnaam en het wachtwoord correct zijn
 	echo "<h1>Welkom u bent ingelog</h1>";
 	echo "<p> <img src='imges/stickers-schatkist.jpg.jpg'></p>";
-} else {
+} else
+	{
 	//als de gebruikersnaam of het wachtwoord onjuist zijn
 	echo "<h1>Onjuiste gebruikersnaam of wachtwoord</h1>";
 	echo "<p> <a href='Oefening5_2.php'>Probeer het opnieuw.</a></p>";
